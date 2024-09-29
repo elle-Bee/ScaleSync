272eb780c9a3ac9010883605c8f37508060484b8
elle-Bee <65856801+elle-Bee@users.noreply.github.com>
2024-09-29 12:30:40 +0530

    changes in homepage

--- app/home_page.go
+++ app/home_page.go
@@ -3,17 +3,11 @@ package app
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
-
-	"ScaleSync/api"
 )
 
 func ShowHomePage() fyne.CanvasObject {
 
-	name := 
-
 	content := container.NewVBox()
-		
-	return content 
-	)
+
+	return content
 }
